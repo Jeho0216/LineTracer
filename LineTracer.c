98fe3bcc6895cbc92a06b69aef80bc1f5f3984f5
Jeho0216 <leejen2003@naver.com>
2016-08-12 16:30:57 +0900

    UART 통신 테스트 코드 작성.

--- LineTracer.c
+++ LineTracer.c
@@ -11,11 +11,31 @@
 #define MotorA 0	//Motor 1,2 A Phase
 #define MotorB 1	//Motor 1,2 B Phase
 
-void portInit(void){	//Port Initalize.
-	DDRA = 0xFF;		//Output IR value.
+void port_Init(void){	//Port Initalize.
+	DDRA = 0xFF;		//UART Com port
 	DDRE = 0xFF;		//Output Motor control signal.
 	DDRD = 0x00;		//Input IR value.
 	DDRG = 0xFF;		//Debug LED
+}
+
+void UART_Init(void){
+	UCSR1A = 0x00;
+	UCSR1B = 0x18;		//RX, TX enable
+	UCSR1C = 0x06;		//Data bit : 8bit
+
+	UBRR1H = 0;
+	UBRR1L = 51;		//baudrate : 19200
+						//error : 0.2%
+}
+
+unsigned char get_data(void){
+	while(!(UCSR1A & 0x80));
+	return UDR1;
+}
+
+void send_data(unsigned char data){
+	while(!(UCSR1A & 0x20));
+	UDR1 = data;
 }
 
 void Motor_1(int phase){		//Left Motor
@@ -35,7 +55,7 @@ void Motor_stop(void){
 int main(void){
 	unsigned char Sensor = 0;		//Storing IR value.
 	unsigned char prevSenesor = 0;	//Previous IR value.
-	portInit();
+	port_Init();
 	
 	while(1){
 		prevSensor = Sensor;		//Storing Previous IR value.
@@ -58,34 +78,41 @@ int main(void){
 			case 1 :		//Turn left
 			case 3 :		
 			PORTG = 0x02;
-			if(previousSensor != Sensor)	//If the black line positions have chaged
+			if(previousSensor != Sensor){	//If the black line positions have chaged
 				Motor_stop();
+				send_data(get_data);
+			}
 			Motor_1(MotorB);
 			break;
 			
 			case 6 :		//Straight
 			PORTG = 0x03;
-			if(previousSensor != Sensor)
+			if(previousSensor != Sensor){
 				Motor_stop();
+				send_data(get_data);
+			}
 			Motor_1(MotorB);
 			Motor_2(MotorA);
 			break;
 			
+			case 12 :		//Turn Right
 			case 8 :		//Turn Right
-			case 12 :		
 			PORTG = 0x01;
-			if(previousSensor != Sensor)
+			if(previousSensor != Sensor){
 				Motor_stop();
+				send_data(get_data);
+			}
 			Motor_2(MotorA);
 			break;
 			
 			default :
 			PORTG = 0x03;
-			if(previousSensor != Sensor)
+			if(previousSensor != Sensor){
 				Motor_stop();
+				send_data(get_data);
+			}
 			Motor_1(MotorB);
 			Motor_2(MotorA);
 			break;
 		}
 	}
-}
